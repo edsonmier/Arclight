28eb3432a4e9feaaf93fe89ecead71063680001c
edsonmier <edson_mier28@hotmail.com>
2022-09-04 19:27:54 -0500

    update: fragment

--- Arclight_Tech/app/src/main/java/com/example/arclight_tech/MainActivity.java
+++ Arclight_Tech/app/src/main/java/com/example/arclight_tech/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.arclight_tech;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomSheetDialogFragment payout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        payout = new PayoutActivity();
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                payout.show(getSupportFragmentManager(), "TAG"); break;
+        }
     }
 }
